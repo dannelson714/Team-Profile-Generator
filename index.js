3fb6b79805831179eb348102db6195d916a39aaa
Daniel Nelson <dan.s.nelson@gmail.com>
2022-04-09 11:38:38 -0400

    Established most logic for classes and subclasses. Created functions to get new employees and prompt the user for new questions. Changed sublclasses so they would initialize without arguments for better functionality.

--- index.js
+++ index.js
@@ -1,76 +1,62 @@
 //Include packages required for this project
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./generateMarkdown.js')
-
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please enter a description of your project',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Please enter instructions for installing your project',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please enter any usage information for your user',
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'Please enter guidelines for users to contribute to this project',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Please enter any instructions for testing',
-    },
-    {
-        type: 'input',
-        name: 'credits',
-        message: 'Please list any credits you would like to include for this README',
-    },
-    {
-        type: 'list',
-        message: 'Please choose a license for your project from the following options',
-        name: 'license',
-        choices: ['MIT','APACHE 2.0','GNU GPL v3','BSD 3','None']
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter your e-mail address',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Please enter your Github username'
-    }
-]
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
 // TODO: Create a function to write README file
 function writeToFile(data) {
     fs.writeFile("README.md", data, (err) => err ? console.log(err) : console.log('Success!'));
 }
 
-// TODO: Create a function to initialize app
+// Create a function to initialize app
 function init() {
+    console.log("Please build your team");
+    const initQuestions = [
+        {
+            type: 'input',
+            name: 'name',
+            message: `What is the team manager's name?`,
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: `What is the team manager's id?`,
+        },
+        
+        {
+            type: 'input',
+            name: 'email',
+            message: `What is the team manager's email?`,
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: `What is the team manager's office number?`,
+        }
+    ]
+
     inquirer
-        .prompt(questions)
-        .then((data) => {
-        var readMe = generateMarkdown(data);
-        writeToFile(readMe);
-     });
+    .prompt(initQuestions)
+    .then((data) => {
+        const manager = new Manager(data.officeNumber, data.name, data.id, data.email);
+        return manager
+    })
+    .then((manager) => {
+        newName = manager.getName()
+
+    })
+        inquirer
+        .prompt(manager)
+        manager.getRole
+    
+    
+    
+    
+    
+    
 }
 
 // Function call to initialize app
